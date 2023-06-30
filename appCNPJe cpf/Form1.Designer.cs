bf36e14ce1fd399c57800cda04d5dad771b00044
lucas64987 <134726721+lucas64987@users.noreply.github.com>
2023-06-30 17:06:06 -0300

    validacao de cpf e cnpj

---
+++ appCNPJe cpf/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace appCNPJe_cpf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Rbtncpf = new System.Windows.Forms.RadioButton();
+            this.Rbtncnpj = new System.Windows.Forms.RadioButton();
+            this.Lblentrada = new System.Windows.Forms.Label();
+            this.TxtCampoEntrada = new System.Windows.Forms.TextBox();
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.BtnCANCELAR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Rbtncpf
+            // 
+            this.Rbtncpf.AutoSize = true;
+            this.Rbtncpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rbtncpf.Location = new System.Drawing.Point(54, 32);
+            this.Rbtncpf.Name = "Rbtncpf";
+            this.Rbtncpf.Size = new System.Drawing.Size(75, 29);
+            this.Rbtncpf.TabIndex = 0;
+            this.Rbtncpf.TabStop = true;
+            this.Rbtncpf.Text = "CPF";
+            this.Rbtncpf.UseVisualStyleBackColor = true;
+            // 
+            // Rbtncnpj
+            // 
+            this.Rbtncnpj.AutoSize = true;
+            this.Rbtncnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Rbtncnpj.Location = new System.Drawing.Point(188, 32);
+            this.Rbtncnpj.Name = "Rbtncnpj";
+            this.Rbtncnpj.Size = new System.Drawing.Size(89, 29);
+            this.Rbtncnpj.TabIndex = 1;
+            this.Rbtncnpj.TabStop = true;
+            this.Rbtncnpj.Text = "CNPJ";
+            this.Rbtncnpj.UseVisualStyleBackColor = true;
+            // 
+            // Lblentrada
+            // 
+            this.Lblentrada.AutoSize = true;
+            this.Lblentrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblentrada.Location = new System.Drawing.Point(12, 132);
+            this.Lblentrada.Name = "Lblentrada";
+            this.Lblentrada.Size = new System.Drawing.Size(92, 25);
+            this.Lblentrada.TabIndex = 2;
+            this.Lblentrada.Text = "entrada";
+            // 
+            // TxtCampoEntrada
+            // 
+            this.TxtCampoEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCampoEntrada.Location = new System.Drawing.Point(119, 132);
+            this.TxtCampoEntrada.Name = "TxtCampoEntrada";
+            this.TxtCampoEntrada.Size = new System.Drawing.Size(311, 29);
+            this.TxtCampoEntrada.TabIndex = 3;
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOK.Location = new System.Drawing.Point(119, 229);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(90, 32);
+            this.BtnOK.TabIndex = 4;
+            this.BtnOK.Text = "OK";
+            this.BtnOK.UseVisualStyleBackColor = true;
+            this.BtnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // BtnCANCELAR
+            // 
+            this.BtnCANCELAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCANCELAR.Location = new System.Drawing.Point(234, 229);
+            this.BtnCANCELAR.Name = "BtnCANCELAR";
+            this.BtnCANCELAR.Size = new System.Drawing.Size(112, 32);
+            this.BtnCANCELAR.TabIndex = 5;
+            this.BtnCANCELAR.Text = "cancelar";
+            this.BtnCANCELAR.UseVisualStyleBackColor = true;
+            this.BtnCANCELAR.Click += new System.EventHandler(this.BtnCANCELAR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(495, 334);
+            this.Controls.Add(this.BtnCANCELAR);
+            this.Controls.Add(this.BtnOK);
+            this.Controls.Add(this.TxtCampoEntrada);
+            this.Controls.Add(this.Lblentrada);
+            this.Controls.Add(this.Rbtncnpj);
+            this.Controls.Add(this.Rbtncpf);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "validação de CPF e CNPJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton Rbtncpf;
+        private System.Windows.Forms.RadioButton Rbtncnpj;
+        private System.Windows.Forms.Label Lblentrada;
+        private System.Windows.Forms.TextBox TxtCampoEntrada;
+        private System.Windows.Forms.Button BtnOK;
+        private System.Windows.Forms.Button BtnCANCELAR;
+    }
+}
+
---
+++ appCNPJe cpf/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace appCNPJe_cpf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            string documento = TxtCampoEntrada.Text.Replace(".", "").Replace("/", "").Replace("-", "");
+
+            if (Rbtncpf.Checked)
+            {
+                if (ValidarCPF(documento))
+                {
+                    MessageBox.Show("CPF válido!", "CPF VALIDADO", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("CPF inválido!", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else if (Rbtncnpj.Checked)
+            {
+                if (ValidarCNPJ(documento))
+                {
+                    MessageBox.Show("CNPJ válido!", "CNPJ VALIDADO", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("CNPJ inválido!", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private bool ValidarCPF(string cpf)
+        {
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+
+            if (TodosDigitosIguais(cpf))
+            {
+                return false;
+            }
+
+            if (!VerificarDigitoVerificador(cpf, 9, 10))
+            {
+                return false;
+            }
+
+            if (!VerificarDigitoVerificador(cpf, 10, 11))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TodosDigitosIguais(string cpf)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                if (cpf[i] != cpf[0])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool VerificarDigitoVerificador(string cpf, int multiplicadorInicial, int multiplicadorFinal)
+        {
+            int soma = 0;
+
+            for (int i = 0; i < multiplicadorInicial; i++)
+            {
+                soma += (cpf[i] - '0') * (multiplicadorInicial + 1 - i);
+            }
+
+            int digitoVerificador = 11 - (soma % 11);
+
+            if (digitoVerificador >= 10)
+            {
+                digitoVerificador = 0;
+            }
+
+            return cpf[multiplicadorInicial] - '0' == digitoVerificador;
+        }
+
+        private bool ValidarCNPJ(string cnpj)
+        {
+            cnpj = cnpj.Replace(".", "").Replace("/", "").Replace("-", ""); // Remove caracteres especiais
+
+            if (cnpj.Length != 14 || !long.TryParse(cnpj, out _))
+                return false;
+
+            int[] multiplicadores1 = new int[] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicadores2 = new int[] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string digitoVerificador = cnpj.Substring(12);
+
+            int soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(cnpj[i].ToString()) * multiplicadores1[i];
+
+            int resto = soma % 11;
+            int primeiroDigito = (resto < 2) ? 0 : 11 - resto;
+
+            if (int.Parse(digitoVerificador[0].ToString()) != primeiroDigito)
+                return false;
+
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(cnpj[i].ToString()) * multiplicadores2[i];
+
+            resto = soma % 11;
+            int segundoDigito = (resto < 2) ? 0 : 11 - resto;
+
+            if (int.Parse(digitoVerificador[1].ToString()) != segundoDigito)
+                return false;
+
+            return true;
+        }
+        
+         
+        
+
+        private void BtnCANCELAR_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
