4aa31e95e29878c6fd1291a74cb11e31ccf57723
lucas64987 <134726721+lucas64987@users.noreply.github.com>
2023-07-03 17:08:57 -0300

    cpf e cnpj atualizado

--- appCNPJe cpf/Form1.Designer.cs
+++ appCNPJe cpf/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.TxtCampoEntrada = new System.Windows.Forms.TextBox();
             this.BtnOK = new System.Windows.Forms.Button();
             this.BtnCANCELAR = new System.Windows.Forms.Button();
+            this.Lblgerador = new System.Windows.Forms.Label();
+            this.Btncpf = new System.Windows.Forms.Button();
+            this.Btncnpj = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Rbtncpf
@@ -64,16 +67,16 @@
             // 
             this.Lblentrada.AutoSize = true;
             this.Lblentrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lblentrada.Location = new System.Drawing.Point(12, 132);
+            this.Lblentrada.Location = new System.Drawing.Point(12, 113);
             this.Lblentrada.Name = "Lblentrada";
-            this.Lblentrada.Size = new System.Drawing.Size(92, 25);
+            this.Lblentrada.Size = new System.Drawing.Size(106, 25);
             this.Lblentrada.TabIndex = 2;
-            this.Lblentrada.Text = "entrada";
+            this.Lblentrada.Text = "entrada :";
             // 
             // TxtCampoEntrada
             // 
             this.TxtCampoEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtCampoEntrada.Location = new System.Drawing.Point(119, 132);
+            this.TxtCampoEntrada.Location = new System.Drawing.Point(119, 113);
             this.TxtCampoEntrada.Name = "TxtCampoEntrada";
             this.TxtCampoEntrada.Size = new System.Drawing.Size(311, 29);
             this.TxtCampoEntrada.TabIndex = 3;
@@ -81,7 +84,7 @@
             // BtnOK
             // 
             this.BtnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnOK.Location = new System.Drawing.Point(119, 229);
+            this.BtnOK.Location = new System.Drawing.Point(146, 161);
             this.BtnOK.Name = "BtnOK";
             this.BtnOK.Size = new System.Drawing.Size(90, 32);
             this.BtnOK.TabIndex = 4;
@@ -92,7 +95,7 @@
             // BtnCANCELAR
             // 
             this.BtnCANCELAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCANCELAR.Location = new System.Drawing.Point(234, 229);
+            this.BtnCANCELAR.Location = new System.Drawing.Point(242, 161);
             this.BtnCANCELAR.Name = "BtnCANCELAR";
             this.BtnCANCELAR.Size = new System.Drawing.Size(112, 32);
             this.BtnCANCELAR.TabIndex = 5;
@@ -100,12 +103,47 @@
             this.BtnCANCELAR.UseVisualStyleBackColor = true;
             this.BtnCANCELAR.Click += new System.EventHandler(this.BtnCANCELAR_Click);
             // 
+            // Lblgerador
+            // 
+            this.Lblgerador.AutoSize = true;
+            this.Lblgerador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblgerador.Location = new System.Drawing.Point(114, 238);
+            this.Lblgerador.Name = "Lblgerador";
+            this.Lblgerador.Size = new System.Drawing.Size(271, 25);
+            this.Lblgerador.TabIndex = 7;
+            this.Lblgerador.Text = "gerador de CPF e CNPJ:";
+            // 
+            // Btncpf
+            // 
+            this.Btncpf.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btncpf.Location = new System.Drawing.Point(119, 288);
+            this.Btncpf.Name = "Btncpf";
+            this.Btncpf.Size = new System.Drawing.Size(120, 34);
+            this.Btncpf.TabIndex = 8;
+            this.Btncpf.Text = "gerar cpf";
+            this.Btncpf.UseVisualStyleBackColor = true;
+            this.Btncpf.Click += new System.EventHandler(this.Btncpf_Click);
+            // 
+            // Btncnpj
+            // 
+            this.Btncnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btncnpj.Location = new System.Drawing.Point(274, 288);
+            this.Btncnpj.Name = "Btncnpj";
+            this.Btncnpj.Size = new System.Drawing.Size(120, 34);
+            this.Btncnpj.TabIndex = 9;
+            this.Btncnpj.Text = "gerar cnpj";
+            this.Btncnpj.UseVisualStyleBackColor = true;
+            this.Btncnpj.Click += new System.EventHandler(this.Btncnpj_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(495, 334);
+            this.Controls.Add(this.Btncnpj);
+            this.Controls.Add(this.Btncpf);
+            this.Controls.Add(this.Lblgerador);
             this.Controls.Add(this.BtnCANCELAR);
             this.Controls.Add(this.BtnOK);
             this.Controls.Add(this.TxtCampoEntrada);
@@ -130,6 +168,9 @@
         private System.Windows.Forms.TextBox TxtCampoEntrada;
         private System.Windows.Forms.Button BtnOK;
         private System.Windows.Forms.Button BtnCANCELAR;
+        private System.Windows.Forms.Label Lblgerador;
+        private System.Windows.Forms.Button Btncpf;
+        private System.Windows.Forms.Button Btncnpj;
     }
 }
 
--- appCNPJe cpf/Form1.cs
+++ appCNPJe cpf/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
@@ -98,7 +100,7 @@ namespace appCNPJe_cpf
 
         private bool ValidarCNPJ(string cnpj)
         {
-            cnpj = cnpj.Replace(".", "").Replace("/", "").Replace("-", ""); // Remove caracteres especiais
+            cnpj = cnpj.Replace(".", "").Replace("/", "").Replace("-", "");
 
             if (cnpj.Length != 14 || !long.TryParse(cnpj, out _))
                 return false;
@@ -130,13 +132,112 @@ namespace appCNPJe_cpf
 
             return true;
         }
-        
-         
-        
+
+
+
 
         private void BtnCANCELAR_Click(object sender, EventArgs e)
         {
             Close();
         }
+        String gerarcpf()
+        {
+            Random rnd = new Random();
+
+            int n1 = rnd.Next(0, 10);
+            int n2 = rnd.Next(0, 10);
+            int n3 = rnd.Next(0, 10);
+            int n4 = rnd.Next(0, 10);
+            int n5 = rnd.Next(0, 10);
+            int n6 = rnd.Next(0, 10);
+            int n7 = rnd.Next(0, 10);
+            int n8 = rnd.Next(0, 10);
+            int n9 = rnd.Next(0, 10);
+
+            int soma1 = n1 * 10 + n2 * 9 + n3 * 8 + n4 * 7 + n5 * 6 + n6 * 5 + n7 * 4 + n8 * 3 + n9 * 2;
+
+            int digitoVerificador1 = soma1 % 11;
+
+            if (digitoVerificador1 < 2)
+            {
+                digitoVerificador1 = 0;
+            }
+
+            else
+            {
+                digitoVerificador1 = 11 - digitoVerificador1;
+            }
+
+            int soma2 = n1 * 11 + n2 * 10 + n3 * 9 + n4 * 8 + n5 * 7 + n6 * 6 + n7 * 5 + n8 * 4 + n9 * 3 + digitoVerificador1 * 2;
+
+            int digitoverificador2 = soma2 % 11;
+
+            if (digitoverificador2 < 2)
+            {
+                digitoverificador2 = 0;
+            }
+            else
+            {
+                digitoverificador2 = 11 - digitoverificador2;
+            }
+
+            return n1.ToString() + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + digitoVerificador1 + digitoverificador2;
+        }
+        private void Btncpf_Click(object sender, EventArgs e)
+        {
+            TxtCampoEntrada.Text = gerarcpf();
+        }
+        string gerarCNPJ()
+        {
+            Random rnd = new Random();
+            int n1 = rnd.Next(0, 10);
+            int n2 = rnd.Next(0, 10);
+            int n3 = rnd.Next(0, 10);
+            int n4 = rnd.Next(0, 10);
+            int n5 = rnd.Next(0, 10);
+            int n6 = rnd.Next(0, 10);
+            int n7 = rnd.Next(0, 10);
+            int n8 = rnd.Next(0, 10);
+            int n9 = 0;
+            int n10 = 0;
+            int n11 = 0;
+            int n12 = 1;
+
+            int soma1 = n1 * 5 + n2 * 4 + n3 * 3 + n4 * 2 + n5 * 9 + n6 * 8 + n7 * 7 + n8 * 6 + n9 * 5 + n10 * 4 + n11 * 3 + n12 * 2;
+
+            int dv1 = soma1 % 11;
+
+            if (dv1 < 2)
+            {
+                dv1 = 0;
+
+            }
+            else
+            {
+                dv1 = 11 - dv1;
+            }
+
+            int soma2 = n1 * 6 + n2 * 5 + n3 * 4 + n4 * 3 + n5 * 2 + n6 * 9 + n7 * 8 + n8 * 7 + n9 * 6 + n10 * 5 + n11 * 4 + n12 * 3 + dv1 * 2;
+
+            int dv2= soma2 % 11;
+
+            if (dv2 < 2)
+            {
+                dv2 = 0;
+            }
+            else
+            {
+                dv2 = 11 - dv2;
+            }
+            return n1.ToString() + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12+ dv1 + dv2;
+
+        }
+
+        private void Btncnpj_Click(object sender, EventArgs e)
+        {
+            TxtCampoEntrada.Text = gerarCNPJ();
+        }
     }
 }
+
+
